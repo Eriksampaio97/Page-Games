831ad980675a0dda78906ed208f8d8c2d1538acc
Erik Sampaio <erik.sampaio@trinto.com.br>
2022-03-28 10:39:36 -0300

    Page content

---
+++ script.js
@@ -0,0 +1,11 @@
+function acao(){
+
+    let mdl = document.querySelector('.mdl')
+
+    mdl.style.display = 'block';
+}
+
+function fechar(){
+    let mdl = document.querySelector('.mdl')
+    mdl.style.display = "none";
+}
